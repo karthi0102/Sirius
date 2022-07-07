2f346740a5ebd6961e60c2ba4e26e42ba1fc5f2b
KARTHIKEYAN R <88583975+karthi0102@users.noreply.github.com>
2022-07-07 23:46:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const blue=require('./blue');
+const orange=require('./orange');
+const red=require('./red');
+const all=[blue,orange,red];
+module.exports=all;
---
+++ red.js
@@ -0,0 +1,4 @@
+module.exports={
+    name:"karthi",
+    color:"red"
+}
